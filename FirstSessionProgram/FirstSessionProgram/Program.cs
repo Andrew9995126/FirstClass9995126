358369276f0ecd2a8f2ca921a426513f50cc7228
Student Test IT Standard <00000@student.toiohomai.ac.nz>
2019-07-24 11:27:28 +1200

    Updated Program

---
+++ FirstSessionProgram/FirstSessionProgram/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstSessionProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please Enter Your First Name:  ");
+            string name = Console.ReadLine();
+            Console.Write("Please Enter Your Last Name:  ");
+            string name1 = Console.ReadLine();
+            Console.WriteLine("Hello {0} {1}, nice to meet you", name,name1);
+            Console.ReadLine();
+            Console.WriteLine("Hello "+name + " " + name1+", nice to meet you");
+            Console.ReadLine();
+            Console.WriteLine($"Hello {name} {name1}, nice to meet you");
+            Console.ReadLine();
+            
+            
+            
+
+
+        }
+    }
+}
